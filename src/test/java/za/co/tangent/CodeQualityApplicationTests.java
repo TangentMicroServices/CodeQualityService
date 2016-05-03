b43a45cdeef180f0dbd1e36a8b4c37356c7e8d4d
Andre <andredutoit84@gmail.com>
2016-05-03 10:13:05 +0000

    Added test to ensure application context starts

--- src/test/java/za/co/tangent/CodeQualityApplicationTests.java
+++ src/test/java/za/co/tangent/CodeQualityApplicationTests.java
@@ -1,16 +1,28 @@
 package za.co.tangent;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CodeQualityApplication.class)
-public class CodeQualityApplicationTests {
+public class CodeQualityApplicationTests implements ApplicationContextAware {
+
+	private ApplicationContext appContext;
 
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(appContext);
+	}
+
+	@Override
+	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+		this.appContext = applicationContext;
 	}
 
 }
